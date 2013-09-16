639569d5515581c83f3177b2933cb4b164773101
Likvidator <max-kor4@yandex.ru>
2013-09-16 10:09:44 +0400

    Finish Programm7 and start Programm8

--- Programm8/Programm8/Program.cs
+++ Programm8/Programm8/Program.cs
@@ -17,18 +17,87 @@ namespace Programm8
         static void Main(string[] args)
         {
             int n, m;
-            n = Input("Введите n:");
-            m = Input("Введите m:");
+            n = Input("Введите n");
+            m = Input("Введите m");
             Random ran = new Random();
             double[,] arr = new double[n, m];
             for (int i = 0; i != n; i++)
                 for (int j = 0; j != m; j++)
                     arr[i, j] = ran.Next(1, 100);
+            double[] a = new double[n];
+            double[] b = new double[m];
+            a = stolb(arr, m, n);
+            b = strok(arr, m, n);
+            double[] sz = new double[n];
+            double[] sx= new double[n];
+            double[] stx = new double[m];
+            double[] stz = new double[m];
+            for (int i = 0; i != n; i++)
+            {
+                for (int j = 0; j != m; j++)
+                {
+                    if (arr[i, j] > a[i])
+                        sz[i]++;
+                    else
+                        sx[i]++;
+                }
+            }
+            for (int j = 0; j != m; j++)
+            {
+                for (int i = 0; i != n; i++)
+                {
+                    if (arr[j, i] > b[j])
+                        stx[i]++;
+                    else
+                        stz[i]++;
+                }
+            }
+
             PrintMatrix(arr, n, m);
+            Printf("Больше элементов в строке", sz);
+            Printf("Меньше элементов в строке", sx);
+            Printf("Больше элементов в столбце", stz);
+            Printf("Меньше элементов в столбце", stx);
+
             Console.ReadKey();
+        }
 
+        private static void Printf(string p, double[] sz)
+        {
+            Console.WriteLine(p);
+            for (int i = 0; i != sz.Length; i++)
+                Console.Write("  {0}",sz[i]);
+            Console.WriteLine("\n");
+        }   
 
+        private static double[] strok(double[,] arr, int m, int n)
+        {
+            double[] a = new double[m];
+            double s = 0;
+            for (int i = 0; i != m; i++)
+            {
+                for (int j = 0; j != n; j++)
+                {
+                    s += arr[j, i];
+                }
+                a[i] = s / m;
+            }
+            return a;
+        }
 
+        private static double[] stolb(double[,] arr, int m, int n)
+        {
+            double[] a = new double[n];
+            double s = 0;
+            for (int i = 0; i != n; i++)
+            {
+                for (int j = 0; j != m; j++)
+                {
+                    s += arr[i, j];
+                }
+                a[i] = s / m;
+            }
+            return a;
         }
 
         private static void PrintMatrix(double[,] arr, int n, int m)
@@ -37,8 +106,9 @@ namespace Programm8
             {
                 Console.Write("\n");
                 for (int j = 0; j != m; j++)
-                    Console.Write("{0}\t", arr[i, j]);
+                    Console.Write("{0}[{1}{2}]\t", arr[i, j], i, j);
             }
+            Console.Write("\n");
         }
 
 
