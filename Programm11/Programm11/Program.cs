51374905faad2f3f6f6dbfd02e7d3bf6fd70695a
Likvidator <max-kor4@yandex.ru>
2013-09-30 09:32:58 +0400

    New programm 11

---
+++ Programm11/Programm11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programm11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MaxN = 5;
+            MPclass[] arr = new MPclass[MaxN];
+        }
+    }
+    
+    class MPclass
+    {
+        
+        double m, p;
+        bool zn;  
+
+        public void Prog()
+        {
+        }
+
+    }
+}
